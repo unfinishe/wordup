513ed4bf6a6549067edbff8ff01a71d7adbad8e0
Thomas Baumhauer <thomas@thomba.de>
2025-11-10 19:22:20 +0100

    Release 1.3.1: Improve Code Quality

--- src/__version__.py
+++ src/__version__.py
@@ -1,8 +1,8 @@
 """Version information for WordUp."""
 
-__version__ = "1.3.0"
-__version_info__ = (1, 3, 0)
+__version__ = "1.3.1"
+__version_info__ = (1, 3, 1)
 
 # Release information
-RELEASE_NAME = "Recap Mode"
+RELEASE_NAME = "Improve Code Quality"
 BUILD_DATE = "2025-11-10"
